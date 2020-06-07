b5b972961387d3aada7b9fe975dbda301f5fdfd1
sho40 <kanazawa.sho40@gmail.com>
2020-06-07 21:17:15 +0900

    initialize

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Container, Form, InputGroup, Input, InputGroupAddon, Button, Table } from 'reactstrap';
+
 
 function App() {
+  const [value, setValue] = useState('')
+  const [todos, setTodos] = useState([])
+  const handleSubmit = e => {
+    e.preventDefault()
+    addTodo(value)
+    setValue('')
+  }
+
+  const addTodo = text => {
+    const newTodos = [...todos, {text, comolete: false}]
+    setTodos(newTodos)
+    console.log(newTodos)
+  }
+
+  const removeTodo = index => {
+    const newTodos = [...todos]
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
+  }
+
+  const completeTodo = index => {
+    const newTodos = [...todos]
+    newTodos[index].complete = !newTodos[index].complete
+    setTodos(newTodos)
+    console.log(newTodos)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1 className="mt-4">Todo リスト</h1>
+        <Form onSubmit={handleSubmit}>
+          <InputGroup>
+            <Input type="text"
+              value={value} 
+              onChange={e => setValue(e.target.value)} />
+            <InputGroupAddon addonType="append">
+              <Button type="submit" color="primary">追加</Button>
+            </InputGroupAddon>
+          </InputGroup>
+        </Form>
+      </Container>
+      <Container>
+        <Table>
+          <tbody>
+            {todos && todos.map((todo, index) => (
+              <tr　key={index}>
+                <th　className="text-left" style={{ textDecoration: todo.complete ? "line-through" : ""}}>
+                  {todo.text}
+                </th>
+                <td className="text-right">
+                  <Button
+                    color={todo.complete ? "secondary" : "success"}
+                    className="mr-2" 
+                    onClick={() => completeTodo(index)}>
+                      {todo.complete ? "完了" : "未完了" }
+                  </Button>
+                  <Button color="danger"　onClick={() => removeTodo(index)}>削除</Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
     </div>
   );
 }
